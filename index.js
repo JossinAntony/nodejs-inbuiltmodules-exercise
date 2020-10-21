04e8ffbd1b7af8114a2259e07f02825afd700167
JossinAntony <jossinde@gmail.com>
2020-10-21 10:42:24 +0200

    inbuilt module path tried.

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ const myAssert = require('assert');
 //myAssert(3>5); assertion false leads to error
 myAssert.strictEqual(50, 50, "Error message");
 //-------------------
-//path
+//path  	
+const myPath = require('path');
+console.log(myPath.basename('S:\Flutter\workspace\nodepgms\modules-inbuilt-exercise\index.js'));
+console.log(myPath.dirname('S:\Flutter\workspace\nodepgms\modules-inbuilt-exercise\index.js'));
+//-------------------
+//  	
