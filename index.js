8342460148955da476b9a643c87f30689c8079a2
JossinAntony <jossinde@gmail.com>
2020-10-21 08:29:41 +0200

    initial commit, assert module tried.

---
+++ index.js
@@ -0,0 +1,8 @@
+//Inbuilt modules- 
+//----------------
+//Assert
+const myAssert = require('assert');
+//myAssert(3>5); assertion false leads to error
+myAssert.strictEqual(50, 50, "Error message");
+//-------------------
+//path
